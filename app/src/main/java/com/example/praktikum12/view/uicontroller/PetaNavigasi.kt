08cad5ec8fc206be250a1c51ad2f2a6a1e505f70
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-18 18:34:39 +0700

    add DataSiswaApp root composable

---
+++ app/src/main/java/com/example/praktikum12/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,13 @@
+package com.example.praktikum12.view.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier
+){
+    HostNavigasi(navController = navController)
+}
