aa8a94130340531b174f4505d327696d296d63db
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-18 18:53:29 +0700

    add Scaffold and TopAppBar to EntrySiswaScreen

--- app/src/main/java/com/example/praktikum12/view/uicontroller/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum12/view/uicontroller/HalamanEntry.kt
@@ -1,8 +1,14 @@
 package com.example.praktikum12.view.uicontroller
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum12.view.route.DestinasiEntry
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -11,5 +17,18 @@ fun EntrySiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EntrySiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    )
 
 }
