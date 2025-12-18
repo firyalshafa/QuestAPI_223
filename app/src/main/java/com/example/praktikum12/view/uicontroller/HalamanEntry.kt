a189bfd25e280f7da9cdcac018eefeb235911311
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-18 19:00:13 +0700

    implement EntrySiswaBody form and save button

--- app/src/main/java/com/example/praktikum12/view/uicontroller/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum12/view/uicontroller/HalamanEntry.kt
@@ -1,17 +1,24 @@
 package com.example.praktikum12.view.uicontroller
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.belajar.R
 import com.example.praktikum12.modeldata.DetailSiswa
 import com.example.praktikum12.modeldata.UIStateSiswa
 import com.example.praktikum12.view.route.DestinasiEntry
@@ -61,6 +68,26 @@ fun EntrySiswaBody(
     onSiswaValueChange: (DetailSiswa) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
-) {}
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        FormInputSiswa(
+            detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button (
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(id = R.string.btn_simpan))
+        }
+    }
+}
+}
 
 }
