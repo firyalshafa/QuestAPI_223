665348d4de90e63a06f8893dc147653bbb0fcb1c
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-18 18:09:23 +0700

    add uiStateSiswa state holder

--- app/src/main/java/com/example/praktikum12/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum12/viewmodel/EntryViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.praktikum12.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.praktikum12.modeldata.UIStateSiswa
 import com.example.praktikum12.repositori.RepositoryDataSiswa
 
 class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
     ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
     }
