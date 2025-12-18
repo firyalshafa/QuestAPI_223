7ba308d417ca4c922353b91ac0c94fa42ae0c237
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-18 18:08:24 +0700

    add EntryViewModel with RepositoryDataSiswa dependency

---
+++ app/src/main/java/com/example/praktikum12/viewmodel/EntryViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum12.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.praktikum12.repositori.RepositoryDataSiswa
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel(){
+
+    }
