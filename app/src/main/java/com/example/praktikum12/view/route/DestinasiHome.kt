fcda404894cf0937d8fbdc46f06ff695eca74e6a
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-18 18:28:17 +0700

    add DestinasiHome route

---
+++ app/src/main/java/com/example/praktikum12/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum12.view.route
+
+
+import com.example.belajar.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
