b7d782579a1020abd968abbfa52b2285cd8e455d
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-18 18:01:03 +0700

    add StatusUiSiswa states (Loading, Success, Error)

--- app/src/main/java/com/example/praktikum12/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum12/viewmodel/HomeViewModel.kt
@@ -14,6 +14,9 @@ import retrofit2.HttpException
 import java.io.IOException
 
 sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
 
 }
 
