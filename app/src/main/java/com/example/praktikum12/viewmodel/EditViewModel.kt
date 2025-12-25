da84406f7d023d89dbfa14299cecfcd765afded1
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-25 12:05:01 +0700

    load siswa detail on init

--- app/src/main/java/com/example/praktikum12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum12/viewmodel/EditViewModel.kt
@@ -5,8 +5,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum12.modeldata.UIStateSiswa
+import com.example.praktikum12.modeldata.toUiStateSiswa
 import com.example.praktikum12.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa
@@ -16,6 +19,11 @@ RepositoryDataSiswa
 
     private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
     init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
 
     }
 
