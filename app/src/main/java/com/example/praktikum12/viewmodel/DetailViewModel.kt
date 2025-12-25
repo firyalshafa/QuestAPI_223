b46d186afee7268df4d461b9dad49360117c8a02
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-25 12:16:04 +0700

    add idSiswa and statusUIDetail state to DetailViewModel

--- app/src/main/java/com/example/praktikum12/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/praktikum12/viewmodel/DetailViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.praktikum12.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.praktikum12.modeldata.DataSiswa
@@ -15,5 +18,9 @@ sealed interface StatusUIDetail {
 class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa
 ): ViewModel()  {
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
 
 }
