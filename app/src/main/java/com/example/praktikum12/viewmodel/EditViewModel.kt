6e53583061840f28997c6bdfb20877edb076e74b
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-25 12:03:45 +0700

    read idSiswa from SavedStateHandle in EditViewModel

--- app/src/main/java/com/example/praktikum12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum12/viewmodel/EditViewModel.kt
@@ -14,4 +14,9 @@ RepositoryDataSiswa
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+
+    }
+
 }
