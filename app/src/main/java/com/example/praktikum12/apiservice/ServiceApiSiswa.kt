d1e074519d4399bdb901b53db8242562760ddba4
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-12-25 11:26:15 +0700

    add ServiceApiSiswa retrofit interface (CRUD siswa)

--- app/src/main/java/com/example/praktikum12/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/praktikum12/apiservice/ServiceApiSiswa.kt
@@ -1,22 +1,27 @@
 package com.example.praktikum12.apiservice
 
 import com.example.praktikum12.modeldata.DataSiswa
-import retrofit2.Response
+
 import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
-    @GET("bacateman.php")
-    suspend fun getsiswa(): List<DataSiswa>
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
 
-    @FormUrlEncoded
     @POST("insertTM.php")
-    suspend fun postsiswa(
-        @Field("nama") nama: String,
-        @Field("alamat") alamat: String,
-        @Field("telpon") telpon: String
-    ): Response<Void>
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id:Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 }
